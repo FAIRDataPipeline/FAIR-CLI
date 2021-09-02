c94b509c36bb5e9005c4f8c2a8d10c7d69d3b314
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2021-09-02 12:44:15 +0100

    Remove TOML dependence from CLI install

--- fair/__init__.py
+++ fair/__init__.py
@@ -52,9 +52,3 @@ from pathlib import Path
 import os
 
 logging.basicConfig()
-
-__version__ = toml.load(
-    os.path.join(
-        Path(os.path.join(os.path.dirname(__file__))).parent, "pyproject.toml"
-    )
-)['tool']['poetry']['version']
