94dce619f5de6dc2c57a34e9d4e8e92c185c925c
Kristian Zarębski <kristian.zarebski@ukaea.uk>
2021-10-15 09:47:35 +0100

    Removed unused fixture

---
+++ tests/test_server.py
@@ -0,0 +1,16 @@
+import pytest
+import pytest_mock
+
+from . import conftest as conf
+import fair.registry.server as fdp_serv
+
+LOCAL_REGISTRY_URL = 'http://localhost:8000/api'
+
+
+@pytest.mark.server
+def test_check_server_running(local_registry: conf.TestRegistry, mocker: pytest_mock.MockerFixture):
+    mocker.patch('fair.common.registry_home', lambda: local_registry._install)
+    assert not fdp_serv.check_server_running()
+    with local_registry:
+        assert fdp_serv.check_server_running()
+
